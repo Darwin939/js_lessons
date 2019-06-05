e70675aaaf8e0fa7e7fc1abfb5ca3f4800291cf5
Darwin939 <51247000+Darwin939@users.noreply.github.com>
2019-06-05 17:53:19 +0600

    Update numToarray.js

--- numToarray.js
+++ numToarray.js
@@ -1,12 +1,11 @@
 
-var units = 0
-var tens = 0
-var hundreds = 0
 function numToArray(num) {
-  hundreds = Math.floor(num/100)
-  tens = (num/10)-hundreds*10
-  units = num -((hundreds*100)+(tens*10))
-  var arr = {"units":units,"tens":tens,"hundreds":hundreds}
+var  hundreds = Math.floor(num/100)
+var  tens = Math.floor((num/10)-hundreds*10)
+var  units = num -((hundreds*100)+(tens*10))
+  var arr = {"units":units,
+  "tens":tens,
+  "hundreds":hundreds}
   print(arr)
 
 }
